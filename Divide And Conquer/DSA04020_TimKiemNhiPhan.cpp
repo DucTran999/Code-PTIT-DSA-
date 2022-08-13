317fadcbb8187dd9c6deedc646e904ecb2b4d933
Đức Trần <74949792+DucTran999@users.noreply.github.com>
2022-08-13 20:33:15 +0700

    Update DSA04020_TimKiemNhiPhan.cpp

--- Divide And Conquer/DSA04020_TimKiemNhiPhan.cpp
+++ Divide And Conquer/DSA04020_TimKiemNhiPhan.cpp
@@ -1,26 +1,36 @@
+/* Contributed by Anh Duc */
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
+
+typedef long long ll;
+typedef vector<int> vi;
+#define sync(); ios_base::sync_with_stdio(0); cin.tie(nullptr);
+#define rs(a); memset(a, 0, sizeof(a));
+
+int findK(int *A, int n, int k){
+	int l = 0, r = n - 1;
+	while(l <= r){
+		int mid = (l + r) >> 1;
+		if(A[mid] == k) return mid + 1;
+		else if(A[mid] < k) l = mid + 1;
+		else r = mid - 1;
+	}
+	return -1;
+}
 
 int main() {
-	ios_base::sync_with_stdio(0);
-
-	int t;
+	sync();
+	int t = 1, n, k;
 	cin >> t;
 	while(t--) {
-		ll n,k;
 		cin >> n >> k;
-		vector<int> a(n);
-		for(int i = 0; i < n; i++)
-			cin >> a[i];
-		int it = lower_bound(a.begin(), a.end(), k)- a.begin();
-		if(it >  -1 && it < n && a[it] == k) {
-			cout << it + 1<< endl;
-		} else {
-			cout << "NO\n";
-		}
+		int A[n]; for(int &i : A) cin >> i;
+		if(findK(A, n, k) > 0) cout << findK(A, n, k);
+		else cout << "NO";
+		cout << endl;
 	}
 
 	return 0;
 }
+
 
