16c9f69fdeb0d1b5f2bdcfc9bda2872335ddb4d9
Đức Trần <74949792+DucTran999@users.noreply.github.com>
2022-08-13 20:45:24 +0700

    Update DSA4021

--- Divide And Conquer/DSA04021_DayXauNhiPhan.cpp
+++ Divide And Conquer/DSA04021_DayXauNhiPhan.cpp
@@ -1,33 +1,36 @@
+/* Contributed by Anh Duc */
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
-ll size[100];
 
+typedef long long ll;
+typedef vector<int> vi;
+#define sync(); ios_base::sync_with_stdio(0); cin.tie(nullptr);
+#define rs(a); memset(a, 0, sizeof(a));
+
+ll f[100];
 void fibo() {
-	size[0] = 0;
-	size[1] = 1;
-	for(int i = 2; i <= 92; i++) {
-		size[i] = size[i-1] + size[i-2];
-	}
+	f[1] = 1; f[2] = 1;
+	for(int i = 3; i < 93; i++) f[i] = f[i-2] + f[i-1];
 }
 
-char findK(int n, long long k) {
-	if(n == 1) return '0';
-	if(n == 2) return '1';
-	if(k > size[n-2])
-		return findK(n-1, k - size[n-2]);
+ll findK(ll n, ll k) {
+	if(n == 1) return 0;
+	if(n == 2) return 1;
+	if(k > f[n-2]) return findK(n-1, k - f[n-2]);
 	return findK(n-2, k);
 }
 
 int main() {
+	sync();
 	fibo();
-	int t;
+	ll t = 1, n, k;
 	cin >> t;
-	ll n, k;
 	while(t--) {
 		cin >> n >> k;
-		cout << findK(n,k)<< endl;
+		cout << findK(n, k) << endl;
 	}
+
 	return 0;
 }
+
 
--- Divide And Conquer/DSA04022_KyTuThuKTrongXau.cpp
+++ Divide And Conquer/DSA04022_KyTuThuKTrongXau.cpp
@@ -1,35 +1,39 @@
+/* Contributed by Anh Duc */
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
 
-ll expo(ll a, ll k) {
+typedef long long ll;
+typedef vector<int> vi;
+#define sync(); ios_base::sync_with_stdio(0); cin.tie(nullptr);
+#define rs(a); memset(a, 0, sizeof(a));
+
+ll expo(ll a, ll b){
 	ll ans = 1;
-	while(k) {
-		if(k&1) ans*=a;
-		k>>=1;
+	while(b){
+		if(b&1) ans*=a;
+		b>>=1;
 		a*=a;
 	}
 	return ans;
 }
 
-char findCharK(ll n, ll k) {
-	ll mid = expo(2, n-1);
-	if(k == mid) return n +'A'-1;
-	if( k < mid) {
-		return findCharK(n-1, k);
-	}
-	return findCharK(n-1, k - mid);
+char findK(ll n, ll k){
+	int mid = expo(2,n-1);
+	if(k == mid) return n+'A'-1;
+	if(k < mid) return findK(n-1, k);
+	return findK(n-1, k - mid);
 }
 
-int main() {
-	int t;
+int main(){
+	sync();
+	ll t = 1, n, k;
 	cin >> t;
-	while(t--) {
-		ll n, k;
+	while(t--){
 		cin >> n >> k;
-		cout << findCharK(n, k) << endl;
+		cout << findK(n, k) << endl;
 	}
-
+	
 	return 0;
 }
+
 
