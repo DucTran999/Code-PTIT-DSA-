32778dc09fbf408b4b5883a1b547b871dda1198d
Đức Trần <74949792+DucTran999@users.noreply.github.com>
2022-08-13 19:36:36 +0700

    Update DSA04016_PhanTuThuK.cpp

--- Divide And Conquer/DSA04016_PhanTuThuK.cpp
+++ Divide And Conquer/DSA04016_PhanTuThuK.cpp
@@ -1,23 +1,26 @@
+/* Contributed by Anh Duc */
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-	int t;
+typedef long long ll;
+typedef vector<int> vi;
+#define sync(); ios_base::sync_with_stdio(0); cin.tie(nullptr);
+#define rs(a); memset(a, 0, sizeof(a));
+
+int main(){
+	sync();
+	int t = 1, n, m, k;
 	cin >> t;
-	while(t--) {
-		int n, m , k;
+	while(t--){
 		cin >> n >> m >> k;
-		vector<int>A(n);
-		vector<int>B(m), ans(m+n);
-		for(int i = 0; i<n; i++) {
-			cin >> A[i];
-		}
-		for(int j = 0; j<m; j++) {
-			cin >> B[j];
-		}
-		merge(A.begin(), A.end(), B.begin(), B.end(), ans.begin());
+		vi A(n), B(m), ans(n+m);
+		for(int &i : A) cin >> i;
+		for(int &i : B) cin >> i;
+		merge(A.begin(),A.end(), B.begin(), B.end(), ans.begin());
 		cout << ans[k-1] << endl;
 	}
+	
 	return 0;
 }
+
 
